73711ce36e3a7a1c82e82e36df31f46bc2bcf279
sudhanshunitinatalkar <atalkarsudhanshu@proton.me>
2026-03-23 22:09:16 +0530

    update

--- modules/hosts/ionix_pbrlab_pi5/configuration/configuration.nix
+++ modules/hosts/ionix_pbrlab_pi5/configuration/configuration.nix
@@ -24,11 +24,73 @@
       extraGroups = [ "wheel" ];
     };
 
+    environment.systemPackages = with pkgs;
+    [
+      tree
+      util-linux
+      vim
+      wget
+      curl
+      git
+      gptfdisk
+      htop
+      pciutils
+      home-manager
+      mosquitto
+      cloudflared
+    ];
+
     time.timeZone = "Asia/Kolkata";
 
     services = {
-      printing.enable = true;
       openssh.enable = true;
+      mosquitto = 
+        {
+          enable = true;
+    
+          listeners = [
+            {
+              port = 3001;
+              address = "127.0.0.1";
+              settings = {
+                protocol = "websockets";
+              };
+              
+              # Users MUST be placed inside the listener they apply to
+              users = {
+                datalogger = {
+                  acl = [ 
+                    "readwrite /datalogger/#"
+                    "read /datalogger_admin/#"
+                  ];
+                  hashedPasswordFile = "/var/lib/mosquitto/datalogger_passwd"; 
+                };
+                
+                datalogger_admin = {
+                  acl = [
+                    "readwrite /datalogger_admin/#"
+                    "readwrite /datalogger/#"
+                  ];
+                  hashedPasswordFile = "/var/lib/mosquitto/datalogger_admin_passwd"; 
+                };
+              };
+            }
+          ];
+        };
+
+        cloudflared = {
+          enable = false;
+          tunnels = {
+            # mqtt
+            "a051417b-c023-4dfb-a862-05eae605fbcf" = {
+              credentialsFile = "/home/pbr/.cloudflared/a051417b-c023-4dfb-a862-05eae605fbcf.json";
+              ingress = {
+                "mqtt.eltros.in" = "http://localhost:3001";        
+              };
+              default = "http_status:404";
+            };
+          };
+        };
     };
 
     environment.systemPackages = with pkgs; [
